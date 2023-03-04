4c78833fa6299bfe61951f82ed1fe8b46925b85d
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-03-04 11:59:02 +0700

    tambah comment lagi di web php

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,17 @@ use App\Http\Controllers\LoginController;
 |
 */
 
+// route untuk login, menampilkan page login
 Route::get('/', [LoginController::class, 'login']);
+// route untuk register, menampilkan page register
 Route::get('/register', [LoginController::class, 'register']);
+// route untuk login, methodnya post, melakukan validasi
 Route::post('/login', [LoginController::class, 'authenticate']);
+// route untuk register, methodnya post, melakukan storing data
 Route::post('/register', [LoginController::class, 'store']);
 
+// route untuk logout
 Route::post('/logout', [LoginController::class, 'logout']);
+
+// route untuk home, menampilkan page home, dan harus login terlebih dahulu, middleware auth untuk mengecek apakah sudah login atau belum
 Route::get('/home', [LoginController::class, 'home'])->middleware('auth');
